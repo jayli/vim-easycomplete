d49c038e6e90015c9b7e22484c1f14a28d588748
bachi <bachi@taobao.com>
2022-03-14 01:58:32 +0800

    add nvim-lsp-installer support for lua #190

--- autoload/easycomplete/installer/lua.sh
+++ autoload/easycomplete/installer/lua.sh
@@ -15,6 +15,25 @@ set -e
 #chmod +x emmylua-ls
 
 
+# On MacOS, use clangd in Command Line Tools for Xcode.
+
+user_root=$(echo $(pwd) | sed -e "s/\/.config.*$//g")
+nvim_lsp_path="$user_root/.local/share/nvim/lsp_servers/sumneko_lua/extension/server/bin"
+
+#---------------------------------------------------------------if
+if test -x $nvim_lsp_path/lua-language-server; then
+  echo "nvim_lsp_config detecting is ok."
+cat <<EOF >sumneko-lua-language-server
+#!/usr/bin/env bash
+$nvim_lsp_path/lua-language-server -E -e LANG=en $nvim_lsp_path/main.lua \$*
+EOF
+  chmod +x sumneko-lua-language-server
+  echo "install ready!"
+  exit
+
+# --------------------------------------------------------------else
+else
+
 os=$(uname -s | tr "[:upper:]" "[:lower:]")
 
 version="2.6.6"
@@ -31,7 +50,6 @@ esac
 package_name="vscode-lua-v$version-$platform"
 
 url="https://github.com/sumneko/vscode-lua/releases/download/v$version/$package_name.vsix"
-# url="https://github.com/sumneko/vscode-lua/releases/download/v$version/lua-$version.vsix"
 echo "downloading $url"
 
 asset="vscode-lua.vsix"
@@ -49,3 +67,6 @@ DIR=\$(cd \$(dirname \$0); pwd)/extension/server
 EOF
 
 chmod +x sumneko-lua-language-server
+
+# --------------------------------------------------------------fi
+fi
