618d5dba6b4b2f3ee5a52c5744400fe2e7f7e73f
jayli <bachi@taobao.com>
2022-03-30 09:54:22 +0800

    code refractor

--- lua/easycomplete/autoload.lua
+++ lua/easycomplete/autoload.lua
@@ -1,24 +1,8 @@
 local Util = require "easycomplete.util"
-local Servers = require("nvim-lsp-installer.servers")
-local Server  = require("nvim-lsp-installer.server")
 local console = Util.console
 local log = Util.log
 local AutoLoad = {}
-
-local function get_configuration()
-  local curr_lsp_name = Util.current_lsp_name()
-  local ok, server = Servers.get_server(curr_lsp_name)
-  return {
-    easy_plugin_ctx = Util.current_plugin_ctx(),
-    easy_plugin_name = Util.current_plugin_name(),
-    easy_lsp_name = curr_lsp_name,
-    easy_lsp_config_path = Util.get_default_config_path(),
-    easy_cmd_full_path = Util.get_default_command_full_path(),
-    nvim_lsp_root = Util.get(server, "root_dir"),
-    nvim_lsp_root_path = Server.get_server_root_path(),
-    nvim_lsp_ok = ok,
-  }
-end
+local get_configuration = Util.get_configuration
 
 local function curr_lsp_constructor_calling()
   Util.constructor_calling_by_name(Util.current_plugin_name())
@@ -145,17 +129,17 @@ AutoLoad.ts = {
 
 AutoLoad.lua = {
   setup = function(self)
+    local configuration = get_configuration()
+    if not configuration.nvim_lsp_ok then
+      return
+    end
     local curr_plugin_ctx = Util.current_plugin_ctx()
     local plugin_name = Util.current_plugin_name()
     local curr_lsp_name = Util.current_lsp_name()
-    local ok, server = Servers.get_server(curr_lsp_name)
-    if not ok then
-      return
-    end
 
     local lua_config_path = Util.get_default_config_path()
-    local lua_cmd_full_path = Util.get_default_command_full_path()
-    local nvim_lsp_root = Util.get(server, "root_dir")
+    local lua_cmd_full_path = configuration.easy_cmd_full_path
+    local nvim_lsp_root = configuration.nvim_lsp_root
     local nvim_cmd_path = vim.fn.join({nvim_lsp_root, "extension", "server", "bin"}, "/")
     local nvim_cmd_bin = "lua-language-server"
     local nvim_lua_script = "main.lua"
