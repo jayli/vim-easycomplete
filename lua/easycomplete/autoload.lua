d3f032feb5fade7ecff348c14841738b870fb128
jayli <bachi@taobao.com>
2022-04-01 20:39:40 +0800

    test ruby failed

--- lua/easycomplete/autoload.lua
+++ lua/easycomplete/autoload.lua
@@ -6,6 +6,51 @@ local get_configuration = Util.get_configuration
 local show_success_message = Util.show_success_message
 local curr_lsp_constructor_calling = Util.curr_lsp_constructor_calling
 
+-- DIR/Gemfile 的引用位置对了，但lsp不生效
+-- AutoLoad.rb = {
+--   setup = function(self)
+--     local configuration = get_configuration()
+--     local gemfile = vim.fn.join({
+--       configuration.nvim_lsp_root,
+--       "gems/solargraph-0.44.3/Gemfile"
+--     }, "/")
+--     local solargraph_path = vim.fn.join({
+--       configuration.nvim_lsp_root,
+--       "bin",
+--       "solargraph"
+--     }, "/")
+--     -- BUNDLE_GEMFILE=$DIR/Gemfile bundle exec ruby $DIR/bin/solargraph $*
+--     Util.create_command(configuration.easy_cmd_full_path, {
+--       "#!/usr/bin/env sh",
+--       vim.fn.join({"BUNDLE_GEMFILE=" .. gemfile,
+--         "bundle",
+--         "exec",
+--         "ruby",
+--         solargraph_path,
+--         "$*"
+--       }, " ")
+--     })
+--     curr_lsp_constructor_calling()
+--     show_success_message()
+--   end
+-- }
+
+AutoLoad.go = {
+  setup = function(self)
+    local configuration = get_configuration()
+    local cmd_path = vim.fn.join({
+      configuration.nvim_lsp_root,
+      'gopls',
+    }, "/")
+    Util.create_command(configuration.easy_cmd_full_path, {
+      "#!/usr/bin/env sh",
+      cmd_path .. " $*",
+    })
+    curr_lsp_constructor_calling()
+    show_success_message()
+  end
+}
+
 AutoLoad.py = {
   setup = function(self)
     local configuration = get_configuration()
