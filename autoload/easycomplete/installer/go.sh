d4c54e343ebebab93fcaaca64158ebe7da7f1750
拔赤 <bachi@taobao.com>
2025-06-12 10:39:55 +0800

    bugfix for go installer

--- autoload/easycomplete/installer/go.sh
+++ autoload/easycomplete/installer/go.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-"$(dirname "$0")/go_install.sh" golang.org/x/tools/gopls@latest
+export GOPROXY=https://mirrors.aliyun.com/goproxy/
+GO111MODULE=on go clean -modcache
+go install golang.org/x/tools/gopls@latest
+# "$(dirname "$0")/go_install.sh" golang.org/x/tools/gopls@latest
+#
