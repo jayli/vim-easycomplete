71f677270e077f32f32d8d2ddd26d808c9ecd1e7
lijing00333 <lijing00333@163.com>
2025-07-26 12:35:56 +0800

    save code for color support for cmdline

--- lua/easycomplete/cmdline.lua
+++ lua/easycomplete/cmdline.lua
@@ -475,11 +475,26 @@ this.REG_CMP_HANDLER = {
       end
     end
   },
+  -- {
+  --   -- highlight colors name
+  --   patterns = {
+  --     "^hi%s.*gui[fb]g=$",
+  --     "^highlight%s.*gui[fb]g=$"
+  --   },
+  --   get_cmp_items = function()
+  --     if this.insearch() then
+  --       return this.get_normal_search_cmp()
+  --     else
+  --       local colors = this.native_colors
+  --       return colors
+  --     end
+  --   end
+  -- },
   {
-    -- highlight
+    -- highlight color groups
     pattern = {
       "^hi%s.*$",
-      "^highlight%s.*$"
+      "^highlight%s.*$",
     },
     get_cmp_items = function()
       if this.insearch() then
@@ -719,6 +734,33 @@ this.cmd_type = {
   syntax    = { 'syntax' },
   user      = { 'user' }
 }
+
+this.native_colors = {
+  "black","dimgray","dimgrey","gray","grey","darkgray","darkgrey","silver",
+  "lightgray","lightgrey","gainsboro","whitesmoke","white","snow","rosybrown",
+  "lightcoral","indianred","brown","firebrick","maroon","darkred","red",
+  "mistyrose","salmon","tomato","darksalmon","coral","orangered","lightsalmon",
+  "sienna","seashell","chocolate","saddlebrown","sandybrown","peachpuff","peru",
+  "linen","bisque","darkorange","burlywood","antiquewhite","tan","navajowhite",
+  "blanchedalmond","papayawhip","moccasin","orange","wheat","oldlace","floralwhite",
+  "darkgoldenrod","goldenrod","cornsilk","gold","lemonchiffon","khaki","palegoldenrod",
+  "darkkhaki","ivory","beige","lightyellow","lightgoldenrodyellow","olive","yellow",
+  "olivedrab","yellowgreen","darkolivegreen","greenyellow","chartreuse","lawngreen",
+  "honeydew","darkseagreen","palegreen","lightgreen","forestgreen","limegreen",
+  "darkgreen","green","lime","seagreen","mediumseagreen","springgreen","mintcream",
+  "mediumspringgreen","mediumaquamarine","aquamarine","turquoise","lightseagreen",
+  "mediumturquoise","azure","lightcyan","aqua","cyan","darkturquoise","cadetblue",
+  "powderblue","lightblue","deepskyblue","skyblue","lightskyblue","steelblue",
+  "aliceblue","dodgerblue","lightslategray","lightslategrey","slategray","slategrey",
+  "lightsteelblue","cornflowerblue","royalblue","ghostwhite","lavender","midnightblue",
+  "navy","darkblue","mediumblue","blue","slateblue","darkslateblue","mediumslateblue",
+  "mediumpurple","rebeccapurple","blueviolet","indigo","darkorchid","darkviolet",
+  "mediumorchid","thistle","plum","violet","purple","darkmagenta","fuchsia","magenta",
+  "orchid","mediumvioletred","deeppink","hotpink","lavenderblush","palevioletred",
+  "crimson","pink","lightpink"
+}
+
+
 
 function this.init_once()
   if vim.g.easycomplete_cmdline ~= 1 then
